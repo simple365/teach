e728428ce52435ef100894e26dfa83b1fdcea92a
TCL\tomluo <tomluo@tcl.com>
2018-11-02 10:21:27 +0800

    添加拦截器项目

--- log-collector/src/main/java/com/tom/business/App.java
+++ log-collector/src/main/java/com/tom/business/App.java
@@ -532,32 +532,6 @@ public class App {
         return eventJson;
     }
 
-
-    static void getString1(int loop_len, int uid_length, int newsid_length) {
-        String logStr = "%s|{\"appkey\":\"browser\", \"he\": {\"userId\": \"%s\", \"area\": \"BR\", \"appVersion\": \"V1.0.2\",\"appTime\": \"%s\"}, \"et\": [{\"eventName\": \"display\", \"kv\": {\"action\": \"1\",\"newsId\": \"%s\"}},"
-                + "{\"eventName\": \"background\",\"kv\": {\"createTime\": \"%s\"}},{\"eventName\": \"foreground\",\"kv\": {\"createTime\": \"%s\"}}]}";
-        String backgroundStr = "%s|{\"appkey\":\"browser\", \"he\": {\"userId\": \"%s\", \"area\": \"BR\", \"appVersion\": \"V1.0.2\",\"appTime\": \"%s\"}, \"et\": [{\"eventName\": \"background\",\"kv\": {\"createTime\": \"%s\"}},{\"eventName\": \"display\", \"kv\": {\"action\": \"2\",\"newsId\": \"%s\"}}]}";
-
-        long millis = System.currentTimeMillis();
-
-        long x = 0;
-        while (x < loop_len) {
-            long forground_createtime = millis - rand.nextInt(99999999);
-            long background_createtime = millis - rand.nextInt(99999999);
-            long log_time = millis - rand.nextInt(99999999);
-            // # 用户id是u，newsid 是 n
-            String user_id = 'u' + getRandomDigits(uid_length);
-            String news_id = 'n' + getRandomDigits(newsid_length);
-            logger.info(String.format(logStr, log_time, user_id, forground_createtime, news_id,
-                    background_createtime, forground_createtime));
-            user_id = 'u' + getRandomDigits(uid_length);
-            news_id = 'n' + getRandomDigits(newsid_length);
-            logger.info(String.format(backgroundStr, log_time, user_id, forground_createtime,
-                    background_createtime, news_id));
-            x++;
-        }
-    }
-
     /**
      * 获取定长度的数字
      *
