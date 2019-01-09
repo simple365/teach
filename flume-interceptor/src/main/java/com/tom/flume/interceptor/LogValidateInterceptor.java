8073b3f78e560a1bbf9de29ef39431f612ff188f
simple365 <aboard123@hotmail.com>
2019-01-09 16:24:23 +0800

    阶段性保存数据

--- flume-interceptor/src/main/java/com/tom/flume/interceptor/LogValidateInterceptor.java
+++ flume-interceptor/src/main/java/com/tom/flume/interceptor/LogValidateInterceptor.java
@@ -8,11 +8,19 @@ import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * 验证用的
+ */
 public class LogValidateInterceptor implements Interceptor {
     public void initialize() {
     }
 
 
+    /**
+     * 有些日志是多行的这种情况，需要将多行拆开成一条一条的日志。
+     * @param event
+     * @return
+     */
     public Event intercept(Event event) {
         String body = new String(event.getBody(), Charset.forName("UTF-8"));
         // body为原始数据，newBody为处理后的数据,判断是否为display的数据类型
